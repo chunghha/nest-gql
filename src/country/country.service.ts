5d5ef6dba75176f9f53a87426a87b356b8f14bb6
Chris Ha <chunghha@users.noreply.github.com>
2018-11-11 18:53:19 -0600

    refactor: use rxjs operator

--- src/country/country.service.ts
+++ src/country/country.service.ts
@@ -1,19 +1,15 @@
 import { HttpService, Injectable } from '@nestjs/common';
 
-import { Country } from '../models/country.model';
+// tslint:disable-next-line
+import { pluck } from 'rxjs/operators';
 
 @Injectable()
 export class CountryService {
   private readonly DATA_SOURCE = 'https://restcountries.eu/rest/v2/all';
-  private countries: Country[] = [];
 
-  constructor(private readonly httpService: HttpService) {}
+  constructor(private readonly httpService: HttpService) { }
 
   public async getCountries() {
-    await this.httpService.get(this.DATA_SOURCE).subscribe(res => {
-      this.countries = res.data;
-    });
-
-    return this.countries;
+    return this.httpService.get(this.DATA_SOURCE).pipe(pluck('data'));
   }
 }
